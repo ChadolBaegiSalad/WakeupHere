22b04169113ab54431ac9f7ccd1ea3516de6b5cb
david <thyun.ahn@gmail.com>
2018-11-26 21:18:28 +0900

    made splash screen for android

--- WakeupHere.js
+++ WakeupHere.js
@@ -1,28 +1,33 @@
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import MapView, { PROVIDER_GOOGLE }from 'react-native-maps';
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View, StatusBar } from "react-native";
+import MapView, { PROVIDER_GOOGLE } from "react-native-maps";
+import SplashScreen from "react-native-splash-screen";
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
 //type Props = {};
 export default class WakeupHere extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar backgroundColor="#4f6d7a" barStyle="light-content" />
         <MapView
-            provider={PROVIDER_GOOGLE}
-            style={styles.map}
-            region={{
-              latitude: 37.78825,
-              longitude: -122.4324,
-              latitudeDelta: 0.015,
-              longitudeDelta: 0.0121,
-            }}
+          provider={PROVIDER_GOOGLE}
+          style={styles.map}
+          region={{
+            latitude: 37.78825,
+            longitude: -122.4324,
+            latitudeDelta: 0.015,
+            longitudeDelta: 0.0121
+          }}
         />
       </View>
     );
@@ -30,6 +35,6 @@ export default class WakeupHere extends Component {
 }
 
 const styles = StyleSheet.create({
-  container: { ... StyleSheet.absoluteFillObject },
+  container: { ...StyleSheet.absoluteFillObject },
   map: { ...StyleSheet.absoluteFillObject }
 });
--- android/app/src/main/java/com/wakeuphere/MainActivity.java
+++ android/app/src/main/java/com/wakeuphere/MainActivity.java
@@ -1,6 +1,10 @@
 package com.wakeuphere;
 
+import android.os.Bundle;
+
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
+
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +15,11 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "WakeupHere";
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
     }
 }
